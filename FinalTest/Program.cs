fef0e6a9cc2722841af871775e75a95760c2f150
mancymani <mancyamani@gmail.com>
2020-11-19 20:31:09 -0500

    first commit

---
+++ FinalTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FinalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adult adult = new Adult();
+            Child child = new Child();
+
+            string Name[] = {}
+            adult.FirstLast();
+
+            child.FirstLasst();
+
+
+
+
+        }
+    }
+
+
+    public class Adult
+    {
+        public string FName = "Joe";
+        public String LName = "Smith";
+
+        public void FirstLast()
+        {
+         //string First = FName;
+         //string Last = LName;
+
+            Console.WriteLine(FName, LName);
+
+        }
+
+    }
+
+    public class Child : Adult
+    {
+        public string FN = "Jacob";
+        public string LN = "Smith";
+
+        public void FirstLasst()
+        {
+        //string First = FN;
+        //string Last = LN;
+            Console.WriteLine(FN, LN);
+
+        }
+    }
+}
+
+    
+
+
+
